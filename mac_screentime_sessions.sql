375eb9bbd38a50ed475892da7aca38b3706ecaf9
Kevin Today <1142185+mieubrisse@users.noreply.github.com>
2025-07-15 23:26:44 -0300

    Update mac_screentime_sessions.sql

--- mac_screentime_sessions.sql
+++ mac_screentime_sessions.sql
@@ -1,7 +1,7 @@
-CREATE TABLE mac_screentime_sessions (
+CREATE TABLE IF NOT EXISTS mac_screentime_sessions (
     ts         TIMESTAMPTZ   PRIMARY KEY,    -- the exact event start time
     duration_s INTEGER       NOT NULL        -- event length in seconds
 );
 
-CREATE INDEX idx_session_durations_ts_date
-    ON session_durations ((ts::date));
+-- We don't need an index because our ts already has a B-key and supports nice ranges
+-- Querying data for a specific data is as simple as WHERE ts >= DATE '2025-07-16' AND ts < DATE '2025-07-17'
